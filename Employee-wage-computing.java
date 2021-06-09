34d1402f2db1bdcfc678b62f5f8c4dadeebe9a28
Pavan-Saketh-Mandha <pavansaketh97@gmail.com>
2021-06-09 22:32:32 +0530

    Calculate Daily Employee Wage - Assume Wage per Hour is 20 - Assume Full Day Hour is 8

--- Employee-wage-computing.java
+++ Employee-wage-computing.java
@@ -1,16 +1,22 @@
 import java.util.*;
-class EmployeewageUC1
+class EmployeewageUC2
 {
 	public static void main(String[] args) 
 	{
 		String greetings="Welcome to Employee Wage Computation Program on Master Branch";
 		System.out.println(greetings);
-		
+		System.out.println("Wage per Hour is 20");
+		System.out.println("Full Day Hour is 8");
+
+		int wage=20;
+                int fullday=8;
+
 		int ispresent = (int)(Math.random() * 3);
 		if (ispresent == 1)
 		{
 			System.out.println("Employee is Present");
-			
+			int dailywage = wage * fullday;
+			System.out.println("Employee Daily Wage is: " + dailywage);
 		}
 		else
 	    {
@@ -18,4 +24,3 @@ class EmployeewageUC1
 		}
     }
 }
-
